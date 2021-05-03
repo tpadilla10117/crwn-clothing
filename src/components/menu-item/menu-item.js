18a616bdf9dfedfa23f1411523c1d61c835362ef
Trinidad Padilla <tpadilla101@live.com>
2021-05-03 10:28:13 -0700

    updates

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -3,11 +3,14 @@ import './menu-item.scss';
 
 const MenuItem =({ title, imageUrl, size }) => (
 
-    <div className={`${size} menu-item`} style={ {
+    <div className={`${size} menu-item`}>
+        <div className="background-image" style={ {
         backgroundImage: `url(${imageUrl})`
-    }}>
+        }}>
+        </div>
+        
         <div className='content'>
-            <h1 className={title}>Hats</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Now</span>
         </div>
     </div>
