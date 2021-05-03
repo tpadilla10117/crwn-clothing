f0625d7b5865053902af6fa641319f1aaf11af9d
Trinidad Padilla <tpadilla101@live.com>
2021-05-03 10:14:11 -0700

    basic card styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepages/homepage.js';
 function App() {
   return (
     <div><HomePage/></div>
---
+++ src/components/menu-item/menu-item.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './menu-item.scss';
+
+const MenuItem =({ title, imageUrl, size }) => (
+
+    <div className={`${size} menu-item`} style={ {
+        backgroundImage: `url(${imageUrl})`
+    }}>
+        <div className='content'>
+            <h1 className={title}>Hats</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+    </div>
+
+)
+
+export default MenuItem;
