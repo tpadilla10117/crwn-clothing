492c8f1c09a2e60bd16945acd4664f0ac2e067d5
Trinidad Padilla <tpadilla101@live.com>
2021-05-06 11:55:42 -0700

    finished and tested the sign-in component

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -4,7 +4,7 @@ import './sign-in.scss';
 import FormInput from '../form-input/form-input.js';
 import CustomButton from '../custom-button/custom-button';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -15,8 +15,20 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+            
+        } catch(error) {
+            console.log(error);
+        }
+
+
         this.setState({ email: '', password:''})
     }
 
