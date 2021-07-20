07d9d1f225916fa02daa1d7d711cec4fb1e078ef
Trinidad Padilla <tpadilla101@live.com>
2021-07-20 09:50:26 -0700

    changed a minor form type

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -53,7 +53,7 @@ class SignIn extends React.Component {
                 
                     <div className='buttons'>
                         <CustomButton type="submit">Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} type="button" isGoogleSignIn>Sign In With Google</CustomButton>
                     </div>
                     
 
