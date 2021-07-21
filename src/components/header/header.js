6f06317ce9f620aa68243e8aa90abe73dcd04ab5
Trinidad Padilla <tpadilla101@live.com>
2021-07-21 16:18:20 -0700

    cart icon added and card-dropdown components added.  Also implemented card-reducer

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'; //higher-order component
 import { auth } from '../../firebase/firebase.utils';
-
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 /* Syntax for importing SVG - we tell create react app that we want a react component that renders an SVG, rather than its filename*/
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.scss';
 
-const Header = ( { currentUser} ) => (
+const Header = ( { currentUser, hidden} ) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
@@ -20,13 +21,17 @@ const Header = ( { currentUser} ) => (
                 currentUser ? <div className="option" onClick={ () => auth.signOut() }>SIGN OUT</div> : <Link className="option" to='/signin'>SIGN IN</Link>
 
             }
+            <CartIcon/>
         </div>
+        {hidden ? null : <CartDropdown />}
+       
     </div>
 );
 
 //Use this to gain access to properties from our redux reducers:
-const mapStateToProps = state => ( {
-    currentUser: state.user.currentUser
+const mapStateToProps = ( {user: { currentUser }, cart: {hidden}}) => ( {
+    currentUser,
+    hidden
 })
 
 //connect() is higher-order component that gets access to 1 of 2 functions:
